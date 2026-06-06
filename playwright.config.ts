ee35d71c8e80ed4ffcd216ed0c79fc223d29d7e4
Dark-Lord777 <207839762+Dark-Lord777@users.noreply.github.com>
2026-06-06 18:18:23 +0300

    fix: edit tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig } from '@playwright/test';
 
-xport default defineConfig({
-  testDir: './tests/',
+export default defineConfig({
+  testDir: './tests',
   timeout: 30000,
   use: {
-    baseUrl: 'http://localhost:8080',
+    baseURL: 'http://localhost:8080',
     screenshot: 'only-on-failure',
-    trace: 'retain-on-failure'.
-  }.
-  projects: [
-  {
-    name: 'chromium',
-    use:  { browserName: 'chromium' },
+    trace: 'retain-on-failure',
   },
+  projects: [
+    {
+      name: 'chromium',
+      use: { browserName: 'chromium' },
+    },
   ],
 });
