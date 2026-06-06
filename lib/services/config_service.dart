a9d7b2b265206e6913eb7e4479e6916b18550caa
Dark-Lord777 <207839762+Dark-Lord777@users.noreply.github.com>
2026-06-06 18:08:46 +0300

    fix: rm dirty imports

--- lib/services/config_service.dart
+++ lib/services/config_service.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/foundation.dart' show kIsWeb; 
 import 'package:wheel_of_fortune/services/config_service_interface.dart';
 import 'package:wheel_of_fortune/services/config_service_worker.dart'; 
-import 'package:wheel_of_fortune/services/config_service_web.dart';
-import 'package:wheel_of_fortune/services/config_service_native.dart';
 
 ConfigServiceInterface createConfigService() {
   return ConfigServiceWorker();
