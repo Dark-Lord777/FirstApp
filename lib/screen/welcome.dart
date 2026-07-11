c475c5c76174830fb1bca1ade189dc88f1e55fe7
Dark-Lord777 <207839762+Dark-Lord777@users.noreply.github.com>
2026-07-11 16:29:45 +0300

    feat: add new icons

--- lib/screen/welcome.dart
+++ lib/screen/welcome.dart
@@ -180,8 +180,8 @@ class _WelcomeScreenState extends State<WelcomeScreen> with TickerProviderStateM
                             child: Column(
                               children: [
                                 const Text(
-                                  'Fortune',
-                                 // 'Wheel of Fortune',
+                                //  'Fortune',
+                                  'Wheel of Fortune',
                                   style: TextStyle(
                                     fontSize: 32,
                                     fontWeight: FontWeight.bold,
